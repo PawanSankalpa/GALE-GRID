53d9b1acb703a9e988e343a4af0fbaa90a3b5ba5
Pawan Sankalpa <Pawansankalpanew123@gmail.com>
2025-05-29 06:14:46 +0530

    solve some errors

--- client/src/useAuth.jsx
+++ client/src/useAuth.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:8000"
-    : "https://gale-grid-1.onrender.com";
+const API_BASE_URL ="https://gale-grid-1.onrender.com";
 
 function useAuth() {
   const [user, setUser] = useState(null);
